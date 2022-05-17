4ed1616f44a3a1324b88f69a3dc80c633f1e04df
AJ <anirban.jana@auto1.com>
2022-05-17 11:23:21 +0200

    Initial Push with koin

--- app/src/main/java/com/auto1/applicationwithkoin/Repository.kt
+++ app/src/main/java/com/auto1/applicationwithkoin/Repository.kt
@@ -1,7 +1,9 @@
 package com.auto1.applicationwithkoin
 
 import com.auto1.data.SampleEntry
+import org.koin.core.annotation.Single
 
+@Single
 class Repository {
     fun getEntry(): List<SampleEntry> {
         return emptyList()
